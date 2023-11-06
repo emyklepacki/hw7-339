bc86c217a7e32f2cb019ad66600ebf007eb3032b
klepacki <klepacki@umich.edu>
2023-11-06 10:08:12 -0500

    new

--- js/video.js
+++ js/video.js
@@ -1,10 +1,15 @@
-var video = document.querySelector("video");
+var video;
 
 window.addEventListener("load", function() {
-	document.querySelector("video").load();
+	video = document.getElementById("player1");
+
+	video.loop = false
+	video.autoplay = false
 });
 
 document.querySelector("#play").addEventListener("click", function() {
+	video.volume = (document.getElementById("slider").value)/100;
+	
 	video.play();
 });
 
@@ -13,11 +18,41 @@ document.querySelector("#pause").addEventListener("click", function() {
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate = 0.9;
+	video.playbackRate = video.playbackRate * .90;
+	// log
+	console.log(video.playbackRate);
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	video.playbackRate = 1.1;
+	video.playbackRate = video.playbackRate * 1.10;
+	// log
+	console.log(video.playbackRate);
 });
 
+document.querySelector("#slider").addEventListener("input", function() {
+	video.volume = (document.getElementById("slider").value)/100;
+});
 
+document.querySelector("#skip").addEventListener("click", function() {
+	let time = video.currentTime + 10;
+	if (time > video.seekable.end(0)){
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime = time;
+	}
+	// log
+	console.log(video.currentTime);
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.className = "oldSchool";
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.className = "video";
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	video.muted = !video.muted;
+});
